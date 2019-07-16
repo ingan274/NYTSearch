543b4a5cd1d3ce933bb78444fb58f9137a27d6ed
ingan <isabelngan27@gmail.com>
2019-07-15 21:27:17 -0700

    js file update

--- assets/js/nyt.js
+++ assets/js/nyt.js
@@ -1,18 +1,26 @@
-$("#search").on("click", function() {
-
-var searchTerm = $("#searchTerm").attr("data-term");
-var searchLimit = $("#searchLimit").attr("data-limit");
-var searchStartY = $("#searchStartY").val();
-var searchFinishY = $("#searchFinishY").val();
-var queryURL = "https://api.nytimes.com/svc/mostpopular/v2/emailed/7.json?q="+searchTerm+"&pub_date="+searchStartY+"&api-key=42mp1WxgpecYIOjpxLnCC9ZE5xxsH4XR"
-
-$.ajax({
+$("#search").on("click", function(event) {
+  event.preventDefault();
+ var searchTerm = $("#searchBox").val()
+ var searchLimit = $("#searchLimit").attr("data-limit");
+ var searchStartY = $("#firstYear").val();
+ var searchFinishY = $("#endYear").val();
+ var queryURL = "https://api.nytimes.com/svc/mostpopular/v2/emailed/7.json?q="+searchTerm+"&pub_date="+searchStartY+"&api-key=42mp1WxgpecYIOjpxLnCC9ZE5xxsH4XR"
+ console.log(searchTerm + searchStartY)
+ $.ajax({
   url: queryURL,
   method: "GET"
-}).then(function(response) {
-
-
-
-})
-
-}
+ }).then(function(response) {
+ 
+ 
+ 
+  $('.clr-btn').on('click', function() {
+     emptySrhResults();
+  });
+ 
+  //empty div
+  function emptySrhResults() {
+     $('.srhResults').empty();
+  }
+ })
+ 
+ })
